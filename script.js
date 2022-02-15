0bbe9b72fc8676e2f80f790b8fd21ef9e4880cf9
codingbuffoon <99515608+codingbuffoon@users.noreply.github.com>
2022-02-15 23:53:06 +0000

    Function practice

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ let sentence = adjective + noun + verb;
 
 // Loop
 let count = 0;
-for (let x = 0; x < Infinity; x++) {
+for (let x = 0; x < 100; x++) {
   count += 2;
 }
 console.log(count);
+
+// Functions
+function sayHello(name, greeting) {
+  console.log(greeting + ", " + name + "!");
+}
+sayHello("Niko", "Yo");
