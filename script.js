243396dbd04e17d1f9a36999cedceaeed3f28ef2
codingbuffoon <99515608+codingbuffoon@users.noreply.github.com>
2022-05-20 22:21:54 +0000

    Added if statement

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
 // Variables
-let adjective = "hairy ";
-let noun = "Bob ";
-let verb = "running";
-let sentence = adjective + noun + verb;
+// let adjective = "hairy ";
+// let noun = "Bob ";
+// let verb = "running";
+// let sentence = adjective + noun + verb;
 
 // Loop
-let count = 0;
-for (let x = 0; x < 100; x++) {
-  count += 2;
-}
-console.log(count);
+// let count = 0;
+// for (let x = 0; x < 100; x++) {
+//   count += 2;
+// }
+// console.log(count);
+// let halp = "hi Leo";
+// for (let i = 0; i <= 100; i += 6) {
+//   console.log(halp);
+// }
 
 // Functions
-function sayHello(name, greeting) {
-  console.log(greeting + ", " + name + "!");
-}
-sayHello("Niko", "Yo");
+// function sayHello(name, greeting) {
+//   console.log(greeting + ", " + name + "!");
+// }
+// sayHello("Niko", "Yo");
+
+// function blahBlah() {
+//   console.log("Blah Blah");
+//   console.log("Blah Blah");
+//   console.log("Blah Blah");
+//   console.log("Blah Blah");
+//   console.log("Blah Blah");
+//   console.log("Blah Blah");
+//   console.log("Blah Blah");
+//   console.log("Blah Blah");
+// }
+// blahBlah();
+
+// While loops
+//let hello = true;
+//while (hello) {
+  // console.log("Leo");
+  // hello = true;
+// }
+
+// If statements
+// const isAwake = false;
+// if (isAwake) {
+//   console.log("Eat");
+// } else {
+//   console.log("Snore");
+// }
+// const string = "hi";
